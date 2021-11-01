aaf2b2b8d311939fd6e583ac885a4a0eb1dfeb65
Austin Bryant <austindbryant@gmail.com>
2021-11-01 16:49:02 -0700

    Tests + view tweaks

--- war_service/war_api/tests.py
+++ war_service/war_api/tests.py
@@ -1,3 +1,149 @@
+import json
+import requests
 from django.test import TestCase
 
-# Create your tests here.
+player1 = json.dumps({"name": "Anna"})
+player2 = json.dumps({"name": "Bill"})
+player3 = json.dumps({"name": "Charlie"})
+game1 = json.dumps({"player1": "Anna", "player2": "Bill"})
+game2 = json.dumps({"player1": "Anna", "player2": "Charlie"})
+play1 = json.dumps({"id": 1})
+play2 = json.dumps({"id": 2})
+
+
+class TestWarAPI(TestCase):
+    def Test_create_player(self):
+        response = requests.post("http://localhost:8000/createplayer/", player1)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"message": "Success"})
+
+        response = requests.post("http://localhost:8000/createplayer/", player2)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"message": "Success"})
+
+    def Test_duplicate_player(self):
+        response = requests.post("http://localhost:8000/createplayer/", player1)
+        self.assertEqual(response.status_code, 404)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"message": "Player Anna already exists"})
+
+    def Test_get_wins_success(self):
+        response = requests.post("http://localhost:8000/getwins/", player1)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"wins": 0})
+
+    def Test_get_wins_failure(self):
+        response = requests.post("http://localhost:8000/getwins/", player3)
+        self.assertEqual(response.status_code, 404)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"message": "Player Charlie not found"})
+
+    def Test_get_games_success(self):
+        response = requests.post("http://localhost:8000/getgames/", player1)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"games_played": 0})
+
+    def Test_get_games_failure(self):
+        response = requests.post("http://localhost:8000/getgames/", player3)
+        self.assertEqual(response.status_code, 404)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"message": "Player Charlie not found"})
+
+    def Test_create_game(self):
+        response = requests.post("http://localhost:8000/startgame/", game1)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"id": 1})
+
+    def Test_create_game_new_player(self):
+        response = requests.post("http://localhost:8000/startgame/", game2)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"id": 2})
+
+    def Test_create_duplicate_game(self):
+        response = requests.post("http://localhost:8000/startgame/", game1)
+        self.assertEqual(response.status_code, 400)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(
+            response_json,
+            {"message": "Game between P1: Anna and P2: Bill already exists"},
+        )
+
+    def Test_take_turn(self):
+        response = requests.post("http://localhost:8000/playgame/", play2)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertIn(
+            response_json,
+            [
+                "finished",
+                "player1",
+                "player2",
+                "player1_card",
+                "player2_card",
+                "pile_size",
+                "message",
+            ],
+        )
+
+    def Test_play_game(self):
+        response = requests.post("http://localhost:8000/playgame/", play1)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertIn(response_json, ["Anna", "Bill"])
+
+    def Test_take_turn_fail(self):
+        response = requests.post("http://localhost:8000/taketurn/", play1)
+        self.assertEqual(response.status_code, 404)
+
+    def Test_play_game_fail(self):
+        response = requests.post("http://localhost:8000/playgame/", play1)
+        self.assertEqual(response.status_code, 404)
+
+    def Test_get_games_success2(self):
+        response = requests.post("http://localhost:8000/getgames/", player1)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"games_played": 1})
+
+        response = requests.post("http://localhost:8000/getgames/", player2)
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response_json, {"games_played": 1})
+
+    def Test_get_wins_success2(self):
+        response1 = requests.post("http://localhost:8000/getwins/", player1)
+        self.assertEqual(response1.status_code, 200)
+        response1_json = json.loads(response1.content.decode("utf-8"))
+
+        response2 = requests.post("http://localhost:8000/getwins/", player2)
+        self.assertEqual(response2.status_code, 200)
+        response2_json = json.loads(response2.content.decode("utf-8"))
+
+        if response1_json["wins"] == 1:
+            self.assertEqual(response2_json, {"wins": 0})
+        else:
+            self.assertEqual(response1_json, {"wins": 0})
+            self.assertEqual(response2_json, {"wins": 1})
+
+    def test_api(self):
+        self.Test_create_player()
+        self.Test_duplicate_player()
+        self.Test_get_wins_success()
+        self.Test_get_wins_failure()
+        self.Test_get_games_success()
+        self.Test_get_games_failure()
+        self.Test_create_game()
+        self.Test_create_game_new_player()
+        self.Test_create_duplicate_game()
+        self.Test_take_turn()
+        self.Test_play_game()
+        self.Test_take_turn_fail()
+        self.Test_play_game_fail()
+        self.Test_get_games_success2()
+        self.Test_get_wins_success2()
--- war_service/war_api/views.py
+++ war_service/war_api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
+from django.core import exceptions
+from django.db import IntegrityError
 import json
 from .models import Player, Game
 from . import war
@@ -8,18 +10,22 @@ from . import war
 
 # Create your views here.
 def create_player(request):
-    print("=================")
-    print(request)
-    print(request.body)
-    print("=================")
     data = json.loads(request.body.decode("utf-8"))
     p = data.get("name")
     player_data = {"name": p}
     try:
         Player.objects.create(**player_data)
         return JsonResponse({"message": "Success"}, status=200)
+    except IntegrityError as e:
+        return JsonResponse(
+            {"message": f"Player {p} already exists"},
+            status=400,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB insert failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB insert failed, {e}"},
+            status=400,
+        )
 
 
 def get_wins(request):
@@ -30,8 +36,16 @@ def get_wins(request):
     try:
         wins = Player.objects.get(name=p).wins
         return JsonResponse({"wins": wins}, status=200)
+    except exceptions.ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"message": f"Player {p} not found"},
+            status=404,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+            status=400,
+        )
 
 
 def get_games(request):
@@ -40,8 +54,16 @@ def get_games(request):
     try:
         games = Player.objects.get(name=p).games_played
         return JsonResponse({"games_played": games}, status=200)
+    except exceptions.ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"message": f"Player {p} not found"},
+            status=404,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+            status=400,
+        )
 
 
 def start_game(request):
@@ -59,8 +81,16 @@ def start_game(request):
     db_game = None
     try:
         db_game = Game.objects.create(**game_entry)
+    except IntegrityError as e:
+        return JsonResponse(
+            {"message": f"Game between P1: {p1} and P2: {p2} already exists"},
+            status=400,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB insert failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB insert failed, {e}"},
+            status=400,
+        )
     return JsonResponse({"id": db_game.id}, status=200)
 
 
@@ -70,8 +100,16 @@ def play_game(request):
     game_json = None
     try:
         game_json = Game.objects.get(id=game_id).game_state
+    except exceptions.ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"message": f"Game {game_id} not found"},
+            status=404,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+            status=400,
+        )
     g = war.Game.from_json(game_json)
     game_response = g.play_game()
 
@@ -80,8 +118,16 @@ def play_game(request):
     db_p1 = None
     try:
         db_p1 = Player.objects.get(name=p1)
+    except exceptions.ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"message": f"Player {p1} not found"},
+            status=404,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+            status=400,
+        )
     db_p1.games_played += 1
     db_p1.wins += game_response["player1_score"]
     db_p1.save()
@@ -89,8 +135,16 @@ def play_game(request):
     db_p2 = None
     try:
         db_p2 = Player.objects.get(name=p2)
+    except exceptions.ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"message": f"Player {p1} not found"},
+            status=404,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+            status=400,
+        )
     db_p2.games_played += 1
     db_p2.wins += game_response["player2_score"]
     db_p2.save()
@@ -103,21 +157,42 @@ def play_game(request):
 def take_turn(request):
     data = json.loads(request.body.decode("utf-8"))
     game_id = data.get("id")
-    game_json = None
+
+    db_game = None
     try:
-        game_json = Game.objects.get(id=game_id).game_state
+        db_game = Game.objects.get(id=game_id)
+    except exceptions.ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"message": f"Game {game_id} not found"},
+            status=404,
+        )
     except Exception as e:
-        return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+        return JsonResponse(
+            {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+            status=400,
+        )
+    game_json = db_game.game_state
     g = war.Game.from_json(game_json)
     game_response = g.game_step()
+    db_game.game_state = g.to_json()
+    db_game.save()
+
     if game_response["finished"] == True:
         p1, p2 = game_response["player1"], game_response["player2"]
 
         db_p1 = None
         try:
             db_p1 = Player.objects.get(name=p1)
+        except exceptions.ObjectDoesNotExist as e:
+            return JsonResponse(
+                {"message": f"Player {p1} not found"},
+                status=404,
+            )
         except Exception as e:
-            return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+            return JsonResponse(
+                {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+                status=400,
+            )
         db_p1.games_played += 1
         db_p1.wins += game_response["player1_score"]
         db_p1.save()
@@ -125,8 +200,16 @@ def take_turn(request):
         db_p2 = None
         try:
             db_p2 = Player.objects.get(name=p2)
+        except exceptions.ObjectDoesNotExist as e:
+            return JsonResponse(
+                {"message": f"Player {p2} not found"},
+                status=404,
+            )
         except Exception as e:
-            return JsonResponse({"message": f"DB query failed, {e}"}, status=409)
+            return JsonResponse(
+                {"exception_type": str(type(e)), "message": f"DB query failed, {e}"},
+                status=400,
+            )
         db_p2.games_played += 1
         db_p2.wins += game_response["player2_score"]
         db_p2.save()
